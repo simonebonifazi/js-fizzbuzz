69b574a31c90926cfdcd91d2da3d63b6610969be
simonebonifazi <simonbonifraz@gmail.com>
2022-06-23 18:07:11 +0200

    adjusted

--- js/script.js
+++ js/script.js
@@ -31,12 +31,14 @@ Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot f
 // let myNumber = '';
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('fizzbuzz')
     } else if (i % 5 == 0) {
         console.log('buzz')
     } else if (i % 3 == 0) {
         console.log('fizz')
+    } else {
+        console.log(i)
     }
 }
